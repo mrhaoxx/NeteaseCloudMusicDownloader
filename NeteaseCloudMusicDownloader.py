11499e08620699245abe537488fa68e538fa8328
haoxingxing <mr.haoxx@gmail.com>
2020-01-23 13:04:24 +0800

    COLORED

--- NeteaseCloudMusicDownloader.py
+++ NeteaseCloudMusicDownloader.py
@@ -4,7 +4,6 @@ from urllib import parse
 import urllib.error
 from eyed3 import id3
 from time import sleep
-import ffmpeg
 import shutil
 import json
 import hashlib
@@ -18,17 +17,13 @@ cloud_music_playlist = '510113940'
 # Dir
 dir_temp="temp/"
 dir_end="music/"
-# Single File (Merge All Music)
-# To be continue ( Mostly not if not neccessary
-isgenerateonefile=True
-isonefileRandom=True
-signalfile="all.mp3"
 # Log
 islog = True
-isverbose = True
+isverbose = False
 isJunkInfo = False
 
-
+import colorama
+from colorama import Fore, Style
 isverbose = islog and isverbose
 isJunkinfo = isJunkInfo and isverbose
 def id_to_url_type_dict(data_all):
@@ -62,7 +57,7 @@ def validateTitle(title):
         print()
     return new_title
 def getFilename(name,artist,typea):       
-    return dir_temp +validateTitle(name)+' - '+validateTitle(artist)+"." + typea;
+    return dir_temp +validateTitle(name)+' - '+validateTitle(artist)+"." + str(typea)
 def GetFileMd5(filename):
     if not os.path.isfile(filename):
         return
@@ -83,9 +78,8 @@ def GetFileMd5(filename):
     return myhash.hexdigest()
 def fetch_api(add):
     if isverbose:
-        print("Fetching API:" + add[:25] + " ...")
+        print("Fetching API:" + add[:25] + "...")
     f = request.urlopen(cloud_music_api + add);
-    
     return f.read().decode('utf-8');
 def resolve_json(str):
     return json.loads(str);
@@ -120,55 +114,53 @@ def download_loop(tracks,trackIds):
         artist = tracks[index]['ar'][0]['name']      
         adl=tracks[index]['al']['name']
         id=tracks[index]['id']
-        isavaible=resolve_json(fetch_api('/check/music?id='+str(id)))
         data_this=data[id]
         ismd5=False
-        if( not isavaible['success']):
-            print(isavaible['message'])
-            pass
         try:
-            disbar(all,i,"[MD5]"+name)
+            disbar(all,i,"[MD5 SUM]"+name)
             if not data_this[2] == GetFileMd5(getFilename(name,artist,data_this[1])):
                 disbar(all,i,'[Download]'+name)
+                isavaible=resolve_json(fetch_api('/check/music?id='+str(id)))
+                if( not isavaible['success']):
+                    print(isavaible)
+                    pass
                 download(data_this[0],getFilename(name,artist,data_this[1]));
             else:
                 disbar(all,i,'[MD5 PASS]'+name)
                 ismd5=True;
-                if islog:
-                    sleep(0.1)
             set_mp3_info(name,artist,getFilename(name,artist,data_this[1]),adl,data_this[1],all,i);
             disbar(all,i,'[Processed]"'+name+'"')
+        except urllib.error.HTTPError as e: 
+            print(e.code)
         except IOError as d :
-            print(d.strerror)
             if islog:
                 print(long_Str_setter(" ",os.get_terminal_size().columns), end="\r")
-                print('[ERROR]'+name);
-                errorinfo[id]=(name);
-                disbar(all,i,'[ERROR]'+name)
+                print(Style.BRIGHT+'['+Fore.RED + 'ERROR'+Style.RESET_ALL+Style.BRIGHT+']'+name);
+                errorinfo[id]=(name,data_this,d.strerror);
+                disbar(all,i,Style.BRIGHT+Fore.RED +'[ERROR]'+name+Style.RESET_ALL)
                 status_failed=status_failed+1;
-        except urllib.error.HTTPError as e:  # 此时是HTTPError
-            print(e.code)
+        
         else:
             print(long_Str_setter(" ",os.get_terminal_size().columns), end="\r")
             if not ismd5:
                 if islog:
-                    print('[SUCCESS]'+name);
+                    print(Style.BRIGHT+'['+Fore.GREEN+'SUCCESS'+Fore.WHITE+']'+name +" "+Fore.YELLOW+str(data_this[1])+Style.RESET_ALL);
             else:
                 if islog:
-                    print('[SUCCESS][Cached]'+name);
-            disbar(all,i,'[SUCCESS]'+name)
-            files[len(files)]=dir_end+validateTitle(name)+' - '+validateTitle(artist)+".mp3";                   
+                    print(Style.BRIGHT+'['+Fore.GREEN+'SUCCESS'+Fore.WHITE+']['+Fore.CYAN+'Cached'+Fore.WHITE+']'+Style.RESET_ALL+Style.DIM+name+" "+Fore.YELLOW+str(data_this[1])+Style.RESET_ALL);
+            disbar(all,i,'[SUCCESS]'+name+" "+str(data_this[1]))
+            files[id]=(name,data_this,dir_end+validateTitle(name)+' - '+validateTitle(artist)+"."+str(data_this[1]));                   
             if islog:
                 status_success=status_success+1;
         i=i+1;
+    print(long_Str_setter(" ",os.get_terminal_size().columns), end="\r")
     if islog:
-        print();
-        print(long_Str_setter("-",os.get_terminal_size().columns))
-        print("All " + str(all) + "; Succeed " + str(status_success) + "; Failed "+ str(status_failed))
+        print(Style.BRIGHT+long_Str_setter("-",os.get_terminal_size().columns))
+        print("All " + str(all) + "; "+Fore.GREEN+"Succeed " + str(status_success) + "; "+Fore.RED+"Failed "+ str(status_failed)+";"+Fore.WHITE)
         if not status_failed == 0:
-            print("Error Music:",errorinfo);
-    #mergemp3s(files)
-    return;
+            print("Error:",Fore.RED,errorinfo,Fore.WHITE)
+        print("Success:",Fore.GREEN,files,Fore.WHITE)
+    return
 def set_mp3_info(name,artist,file,adl,type,all,i): 
     if isJunkInfo:
         disbar(1,0,"Setting up mp3 info")
@@ -181,43 +173,26 @@ def set_mp3_info(name,artist,file,adl,type,all,i):
     tag.title = name;
     tag.album=adl;   
     tag.save(encoding='utf-8');
-    return;
-def mergemp3s(files):
-    #ffmpeg -i "concat:${files:1}" -loglevel panic -c:a copy -c:v copy -f s16le -ar 22.05k -ac 1 ${MusicDir}/all.wav
-    streamlist={}
-    for path in files:
-        streamlist[len(streamlist)]=ffmpeg.input(path);
-    ffmpeg.concat(ffmpeg.input(files[0]),ffmpeg.input(files[1])).output("test.wav").run();
-    print(t);
     return
+
 def main():
-    if isverbose:
-        disbar(5,1,"Getting PlayList")
-        print()
-    get = get_playlist(cloud_music_playlist);
-    if isverbose:
-        disbar(5,2,"Getting Tracks")
-        print()
-    tracks = get_tracks_info(get);
-    list = "";
+    if islog:
+        print(Style.BRIGHT+"Getting PlayList")
+    get = get_playlist(cloud_music_playlist)
+    if islog:
+        print("Getting Tracks")
+    tracks = get_tracks_info(get)
+    list = ""
     if islog:
         print("All " + str(len(tracks)) + " musics" )    
-    if isverbose:
-        disbar(5,2,"Getting TrackIds")
-        print()
     tracksIds = get_tracks_ids(get);
     if islog:
-        print(long_Str_setter("-",os.get_terminal_size().columns))
-    if isverbose:
-        disbar(5,3,"Starting download")
-        print()
+        print(Style.BRIGHT+long_Str_setter("-",os.get_terminal_size().columns))
+        print("Starting download"+Style.RESET_ALL)
     download_loop(tracks,tracksIds);
     if islog:
-        print(long_Str_setter("-",os.get_terminal_size().columns))
-    if isverbose:
-        disbar(5,4,"Done")
-        print()
-    return;
+        print(Style.BRIGHT+long_Str_setter("-",os.get_terminal_size().columns)+Style.RESET_ALL)
+    return
 
 if isverbose:
     disbar(5,0,"Lauching")
@@ -227,11 +202,11 @@ if not os.path.exists(dir_temp):
 if not os.path.exists(dir_end):
     os.mkdir(dir_end)
 if islog:
-    print(long_Str_setter("-",os.get_terminal_size().columns))
+    print(Style.BRIGHT+long_Str_setter("-",os.get_terminal_size().columns))
     print("    NeteaseCloudMusic Downloader     ")
     print("               Powered by haoxingxing")
-    print(long_Str_setter("-",os.get_terminal_size().columns))
-    print("Api:" + cloud_music_api)
-    print("PlayListId:" + cloud_music_playlist.__str__())
-    print(long_Str_setter("-",os.get_terminal_size().columns))
-main();
+    print(Style.BRIGHT+long_Str_setter("-",os.get_terminal_size().columns))
+    print(Fore.CYAN+"Api:" + cloud_music_api+Fore.WHITE)
+    print(Fore.YELLOW+"PlayListId:" + cloud_music_playlist.__str__()+Fore.WHITE)
+    print(Style.BRIGHT+long_Str_setter("-",os.get_terminal_size().columns)+Style.RESET_ALL)
+main()
